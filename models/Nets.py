9a1afe45bc98376e942b880875a5f2a0c0c93f7f
sepehr <sepehrrezaei29@gmail.com>
2022-02-21 19:16:10 +0330

    add_emnist_model

--- models/Nets.py
+++ models/Nets.py
@@ -124,26 +124,46 @@ class CNNMNIST(nn.Module):
 
 class CNNEmnistStd5(nn.Module):
     """438,074: """
+    # def __init__(self, args):
+    #     super(CNNEmnistStd5, self).__init__()
+    #     self.conv1 = nn.Conv2d(1, 32, 3, padding=1, bias=False)
+    #     self.pool1 = nn.MaxPool2d(2, 2)
+    #     self.conv2 = nn.Conv2d(32, 64, 3, padding=1, bias=False)
+    #     self.conv3 = nn.Conv2d(64, 64, 3, padding=1, bias=False)
+    #     self.pool2 = nn.MaxPool2d(2, 2)
+    #     self.conv4 = nn.Conv2d(64, 64, 3, padding=1, bias=False)
+    #     self.conv5 = nn.Conv2d(64, 64, 3, padding=0, bias=False)
+    #     self.pool3 = nn.MaxPool2d(2, 2)
+    #     self.fc1 = nn.Linear(64 * 3 * 3, 512)
+    #     self.fc2 = nn.Linear(512, 26)
+
+    # def forward(self, x):
+    #     x = self.pool1(F.relu(self.conv1(x)))
+    #     x = F.relu(self.conv2(x))
+    #     x = self.pool2(F.relu(self.conv3(x)))
+    #     x = F.relu(self.conv4(x))
+    #     x = self.pool3(F.relu(self.conv5(x)))
+    #     x = x.view(-1, 64 * 3 * 3)
+    #     x_out = F.relu(self.fc1(x))
+    #     x = self.fc2(x_out)
+    #     return F.log_softmax(x, dim=1)
     def __init__(self, args):
         super(CNNEmnistStd5, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=1, bias=False)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1, bias=False)
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=1, bias=False)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.conv4 = nn.Conv2d(64, 64, 3, padding=1, bias=False)
-        self.conv5 = nn.Conv2d(64, 64, 3, padding=0, bias=False)
-        self.pool3 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 3 * 3, 512)
-        self.fc2 = nn.Linear(512, 26)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.fc = nn.Linear(7*7*32, 10)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = F.relu(self.conv2(x))
-        x = self.pool2(F.relu(self.conv3(x)))
-        x = F.relu(self.conv4(x))
-        x = self.pool3(F.relu(self.conv5(x)))
-        x = x.view(-1, 64 * 3 * 3)
-        x_out = F.relu(self.fc1(x))
-        x = self.fc2(x_out)
-        return F.log_softmax(x, dim=1)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
