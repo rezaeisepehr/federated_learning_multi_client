0628e16bb2763b90d76d231e6b522cd669d3f3b7
sepehr <sepehrrezaei29@gmail.com>
2022-02-21 19:21:26 +0330

    emnist_bugfix

--- models/Nets.py
+++ models/Nets.py
@@ -160,10 +160,10 @@ class CNNEmnistStd5(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2))
         self.fc = nn.Linear(7*7*32, 10)
-
+        
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
-        out = out.view(-1, 64 * 3 * 3)
+        out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
